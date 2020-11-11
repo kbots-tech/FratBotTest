14244841de8aa3a90bb91943583de15b37add6c8
mcurranseijo <68172072+mcurranseijo@users.noreply.github.com>
2020-11-10 20:10:03 -0600

    Update autoreact.py
    LINTED YAY

--- cogs/autoreact.py
+++ cogs/autoreact.py
@@ -3,19 +3,17 @@ from discord.ext import commands
 
 
 class Tickets(commands.Cog, name='autoreact'):
-  '''These are the developer commands'''
+    '''These are the developer commands'''
 
-  def __init__(self, bot):
-	  self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.Cog.listener()
-  async def on_message(self, message):
-      if message.channel.id == suggestion_channel:
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id == suggestion_channel:
             await message.add_reaction("✅")
             await message.add_reaction("❌")
 
-	
-
 
 def setup(bot):
-	bot.add_cog(Tickets(bot))
+    bot.add_cog(Tickets(bot))
